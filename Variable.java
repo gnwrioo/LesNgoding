76665dfb23d7974654ae3c2c41e3074c06070f4b
Gunawan Rio <113000165+gnwrioo@users.noreply.github.com>
2022-09-07 04:43:28 +0000

    Add Variable sintaks in java

---
+++ Variable.java
@@ -0,0 +1,16 @@
+class Variable{
+
+  public static void main(String[] args){
+    String nama = "rio";
+
+    System.out.println("halo "+nama);
+
+    String angka2 = "2";
+    int int2 = 2;
+
+    System.out.println("Operasi menggunakan String : "+2+angka2);
+
+    System.out.print("Operasi menggunakan Integer : ");
+    System.out.println(2+int2);
+  }
+}
