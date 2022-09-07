d29d69c0f8690ef43cedcab3f2c21f2cc4689b8d
Gunawan Rio <113000165+gnwrioo@users.noreply.github.com>
2022-09-07 03:59:47 +0000

    add name rio

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+
+    
+    System.out.println("gnwnrio");
+    System.out.println("Rio");
   }
 }
